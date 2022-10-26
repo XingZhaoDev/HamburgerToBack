ccf15118c7cf90a05bcba4e84a301999da466731
Xing Zhao <zhaoxing8325@gmail.com>
2022-10-26 09:37:31 -0700

    Initial Commit

---
+++ HamburgerToBack/HamburgerToBackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HamburgerToBackApp.swift
+//  HamburgerToBack
+//
+//  Created by XING ZHAO on 2022-10-26.
+//
+
+import SwiftUI
+
+@main
+struct HamburgerToBackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
